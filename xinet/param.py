bb30ed24e8cd0b6c74821ca7ad10cb72a6947db7
xinetzone <xinetzone@dingtalk.com>
2020-07-07 14:31:00 +0800

    添加对 tkinter & PyQt5 & PySide2 的支持

--- xinet/param.py
+++ xinet/param.py
@@ -1,10 +1,14 @@
 class ParamDict(dict):
-    def __init__(self, *args, **kw):
+    def __init__(self, custom_type=None, *args, **kw):
         super().__init__(*args, **kw)
+        self.custom_type = custom_type if custom_type else None
 
     def __set__(self, instance, value):
         #print('===> set', instance, value)
-        self[instance] = value
+        if self.custom_type:
+            self[instance] = self.custom_type(value)
+        else:
+            self[instance] = value
 
     def __get__(self, instance, owner):
         return self[instance]
